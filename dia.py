5a2971003d1b37f3409b26e0c8a3c7d804f62d3a
Serch Taylor <94362009+SerchTaylor@users.noreply.github.com>
2026-03-06 10:15:41 +0100

    Bug fix and update with Python

--- dia.py
+++ dia.py
@@ -9,16 +9,17 @@ dia_ini_git = date(2026, 2,11)
 dia_git = date(2026, 2,19)
 dia_ini_go = date(2026, 2,20)
 dia_go = date(2026, 2,27)
-
+dia_ini_python = date(2026, 3, 6)
+dia_python = date(2026, 5, 8)
 
 year = date.today().year
-cumple = date(year, 2, 23)
-cumple2 = date(year+1, 2, 23)
+cumple = date(year+1, 2, 23)
 
-diacumple = cumple2-cumple
+diacumple = cumple - date.today()
 
 falta = dia_git - dias
 falta2 = dia_go - dias
+falta3 = dia_python - dias
 
 git = dia_git-dia_ini_git
 go = dia_go-dia_ini_go
@@ -33,9 +34,17 @@ pag_falta = pag-pag_actual
 pag_go = 411
 pag_actual_go = 301
 pag_falta_go = pag_go-pag_actual_go
-tema_actual = 12
+tema_go = 0
+tema_actual = 134
 tema_total = 34
 tema = tema_total - tema_actual
+
+#paginas de python
+pag_python = 411
+pag_actual_python = 0
+python = pag_python - pag_actual_python
+
+tema_python = 11
 
 print("\nHoy es", dias.strftime("%A %d-%m-%Y"),"\n")
 if (falta.days >= 2 ):
@@ -57,25 +66,43 @@ else:
 if (falta2.days >= 2 ):
     print("Del curso de go!!")
     print("Faltan",falta2.days, "dias, para acabar el curso de GO!!")
-    print("Nos faltan:",pag_falta_go,"para acabar el temario de GO!!\n")
-    print("Nos faltan",tema,"temas para acabar el temario de GO!!\n")
-elif (falta.days == 1 ):
+    print("Nos faltan:",pag_go,"para acabar el temario de GO!!\n")
+    print("Nos faltan",tema_go,"temas para acabar el temario de GO!!\n")
+elif (falta2.days == 1 ):
      print("Del curso de Git / Github!!")
      print("Nos queda",falta2.days, "dia, para acabar el curso de GO!!")
-     print("Nos faltan:",pag_falta_go,"para acabar el temario de GO!!\n")
-     print("Nos faltan",tema,"temas para acabar el temario de GO!!\n")
+     print("Nos faltan:",pag_go,"para acabar el temario de GO!!\n")
+     print("Nos faltan",tema_go,"temas para acabar el temario de GO!!\n")
 elif (falta2.days == 0 ):
     print("Hoy es",dias.strftime("%A %d-%m-%Y"),",y es el último dia del curso de GO!!")
-    print("Nos faltan:",pag_falta_go,"para acabar el temario de GO!!\n")
-    print("Nos faltan",tema,"temas para acabar el temario de GO!!\n")
+    print("Nos faltan:",pag_go,"para acabar el temario de GO!!\n")
+    print("Nos faltan",tema_go,"temas para acabar el temario de GO!!\n")
 else:
     print("Has acabado el curso de GO!!")
-    print("Fueron",go.days,"días, con sus",pag_actual,"páginas\n")
-    print("Fueron",go.days,"días, con sus",tema,"temas\n")
+    print("Fueron",go.days,"días, con sus",pag_go,"páginas",tema_go,"temas\n")
 
+
+if (falta3.days >= 2 ):
+    print("Del curso de Python!!")
+    print("Faltan",falta3.days, "dias, para acabar el curso de Python!!")
+    print("Nos faltan:",python,"para acabar el temario de Python!!\n")
+    print("Nos faltan",tema_python,"temas para acabar el temario de Python!!\n")
+elif (falta3.days == 1 ):
+     print("Del curso de Git / Github!!")
+     print("Nos queda",falta3.days, "dia, para acabar el curso de Python!!")
+     print("Nos faltan:",python,"para acabar el temario de Python!!\n")
+     print("Nos faltan",tema_python,"temas para acabar el temario de Python!!\n")
+elif (falta3.days == 0 ):
+    print("Hoy es",dias.strftime("%A %d-%m-%Y"),",y es el último dia del curso de Python!!")
+    print("Nos faltan:",python,"para acabar el temario de Python!!\n")
+    print("Nos faltan",tema_python,"temas para acabar el temario de Python!!\n")
+else:
+    print("Has acabado el curso de Python!!")
+    print("Fueron",python.days,"días, con sus",pag_actual_python,"páginas\n")
+    print("Fueron",python.days,"días, con sus",tema_python,"temas\n")
 
 if(cumple == dias): print("Muchas Felicidades!!!!\n")
 else:print("Aun no es tu cumple, faltan",diacumple.days,"dias.\n")
 
 
-print("Proximos cursos: Linux1 17 Marzo, Robotica el 23 Marzo y Python el 6 de Marzo durante 6 viernes.\n\n")
+print("Proximos cursos: Linux1 17 Marzo y Robotica el 23 Marzo o el de nivel 3 de programacion Web el 23 marzo.\n\n")
